d04f0cd95e39d635888fc352db06b2194f1a73ae
Billy Ceskavich <bceskavich@gmail.com>
2014-09-17 20:59:04 -0400

    Nav w/ parallax affects completed.

--- static/js/_main.js
+++ static/js/_main.js
@@ -1,15 +1,20 @@
 // Sticks the homepage navbar to the top when scrolled past
 function navAppear(){
   var height = $(window).height();
+  var navHeight = $("nav").height();
 
   $(window).scroll(function(){
     var scroll = $(document).scrollTop();
 
     if (scroll >= (height)) {
       $('nav').addClass('fixed');
+      $('#first').css("top", function(){
+        return navHeight + "px";
+      });
     }
     else {
       $('nav').removeClass('fixed');
+      $('#first').css("top", "0px");
     }
   });
 };
